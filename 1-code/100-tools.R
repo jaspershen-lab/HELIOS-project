a183eb446d26d850a307afc41ffb57f68e33bb95
jaspershen <shenxt1990@outlook.com>
2024-06-11 23:38:06 -0700

    colors for demongraphics

--- 1-code/100-tools.R
+++ 1-code/100-tools.R
@@ -4,4 +4,17 @@ library(ggplot2)
 library(ggsci)
 library(scales)
 
-gender_colors <- c("F" = "pink", "M" = "blue")
+gender_colors <- c("F" = "#00A1D5FF", "M" = "#B24745FF")
+
+ethnicity_colors <-
+  c(
+    "C" = "#B24745FF",
+    "I" = "#79AF97FF",
+    "M" = "#DF8F44FF",
+    "O" = "#80796BFF"
+  )
+
+#
+#
+# scales::show_col(ggsci::pal_jama()(7))
+# ggsci::pal_jama()(7)
